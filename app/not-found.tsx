438a0c6e6d18bb1db78805ba8eaec0fbdf25cc33
H <dmsgktn0208@gmail.com>
2026-02-01 15:23:04 +0900

    feat: 메타데이터 적용

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,15 @@
 // https://nextjs.org/docs/app/api-reference/file-conventions/not-found
 // not-found.tsx는 특정 페이지를 찾을 수 없을 때 에러 페이지로 NextJs 규칙으로 예약된 에러 페이지.
 
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: "Not Found",
+    description: "페이지를 찾을 수 없습니다.",
+};
 
 export default function NotFound() {
-
     return (
         <div>
             <h1 className="red">페이지를 찾을 수 없습니다.</h1>
