b9e4e51d2291a2154d49394a2e340c5ce0deb68a
H <dmsgktn0208@gmail.com>
2026-02-02 16:57:53 +0900

    feat: 게시글 상세 조회

---
+++ board/app/detail/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+
+// 일반적으로 함수에 파라미터를 넘기는건 데이터 전달용, Props를 넘기고 받는건 렌더링을 위한 리액트 전용 파라미터
+type Props = {
+    params: Promise<{ id: string }>;
+};
+
+
+export default async function Detail(props: Props) {
+    const params = await props.params;
+    const db = (await connectDB).db('baord');
+
+    const result = await db.collection('post').findOne({
+        _id: new ObjectId(params.id)
+    });
+
+    if (!result) return <div>게시글을 찾을 수 없습니다.</div>;
+
+    return (
+        <div>
+            <h4>상세페이지임</h4>
+            <h4>{result.title}</h4>
+            <p>{result.content}</p>
+        </div>
+    )
+}
--- board/app/list/page.tsx
+++ board/app/list/page.tsx
@@ -1,4 +1,6 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link"
+import DetailLink from "../detail/[id]/DetailLink";
 
 export default async function List() {
 
@@ -12,8 +14,14 @@ export default async function List() {
             {
                 result.map((a, i) =>
                     <div className="list-item" key={i}>
-                        <h4>{result[i].title}</h4>
-                        <p>{result[i].content}</p>
+                        {/* Link 태그도 prefetch 방식으로 동작한다고함 끄는건 prefetch={false} */}
+                        {/* <Link href={`/detail/${a._id.toString()}`}>
+                            <h4>{a.title}</h4>
+                        </Link> */}
+                        <h4>{a.title}</h4>
+                        <p>{a.content}</p>
+                        {/* id를 props로 전달 */}
+                        <DetailLink id={a._id.toString()} />
                     </div>
                 )
             }
